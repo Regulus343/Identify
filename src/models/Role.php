892ee3568c14cb3840fea4f570330c3cc9db1751
Cody Jassman <codyjassman@gmail.com>
2014-01-01 20:08:41 -0700

    updated Role model - users() changed to belongsToMany() relationship

--- src/models/Role.php
+++ src/models/Role.php
@@ -36,7 +36,7 @@ class Role extends Eloquent {
 	 */
 	public function users()
 	{
-		return $this->hasMany('Regulus\Identify\User', 'role_id')
+		return $this->belongsToMany('Regulus\Identify\User', Config::get('identify::tablePrefix').'user_roles')
 			->orderBy('username');
 	}
 
