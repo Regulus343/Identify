4c2848494fa1b65ff82d8ab467fdd0c55c1c38e8
Rudi van der Merwe <wildfirewolf@gmail.com>
2014-04-23 16:12:31 +0200

    Added methods to User modal for upgrading to Laravel 4.1.26

--- src/models/User.php
+++ src/models/User.php
@@ -248,4 +248,19 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		})->first();
 	}
 
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
 }
