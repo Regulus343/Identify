e1833c39da8985a4ccaaad9fa523c24d517b1cc5
Cody Jassman <me@codyjassman.com>
2016-06-02 18:24:19 -0600

    reversed migrations order on install command and added password_resets migration

--- src/Identify.php
+++ src/Identify.php
@@ -6,8 +6,8 @@
 		and user states. Allows simple or complex user access control implementation.
 
 		created by Cody Jassman
-		v0.9.8
-		last updated on June 1, 2016
+		v0.9.9
+		last updated on June 2, 2016
 ----------------------------------------------------------------------------------------------------------*/
 
 use Illuminate\Auth\SessionGuard;
